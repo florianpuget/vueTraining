bf4bde87d59580fbbff0c8e59859144e4f5739b0
Florian Puget <florian.puget@gmail.com>
2019-11-19 16:23:38 +0100

    ex lesson 12 - child emit event and parent reacts

--- main.js
+++ main.js
@@ -1,20 +1,16 @@
+Vue.component('coupon', {
+    template: `<input placeholder="Enter coupon code" @blur="$emit('applied')">`,
+})
+
 new Vue({
     el: '#root',
     data: {
-        users: [
-            { name: "Ewa", age: "31", brands: "a, b, c" },
-            { name: "Flo", age: "31", brands: "d, e, f" }
-        ],
-        newUser: { name: "", age: "", brands: ""},
-        currentUser: {},
+        couponApplied: false
     },
     methods: {
-        addUser() {
-            this.users.push(this.newUser);
-            this.newUser = { name: "", age: "", brands: ""};
-        },
-        setUser(index) {
-            this.currentUser = this.users[index];
+        onCouponApplied() {
+            this.couponApplied = true;
         }
     }
 });
+
