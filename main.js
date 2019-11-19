99df177524d36a24a3772f9206a62e5d08b74cfa
Florian Puget <florian.puget@gmail.com>
2019-11-19 17:11:02 +0100

    ex lesson15 - inline-template to add reactivity without component, especially if single use

--- main.js
+++ main.js
@@ -1,30 +1,11 @@
-Vue.component('modal', {
-    template: `
-        <div class="modal is-active">
-            <div class="modal-background"></div>
-            <div class="modal-card">
-                <header class="modal-card-head">
-                    <p class="modal-card-title">
-                        <slot name='header'></slot>
-                    </p>
-                    <button class="delete" aria-label="close"></button>
-                </header>
-                <section class="modal-card-body">
-                    <slot></slot>
-                </section>
-                <footer class="modal-card-foot">
-                    <slot name='footer'>
-                        <button class="button is-success">Save changes</button>
-                        <button class="button">Cancel</button>
-                    </slot>
-                </footer>
-            </div>
-        </div>
-    `,
+Vue.component('progress-view', {
+    // no template here, see inline-template in the html
+    data()  {
+        return { completionRate: 0 };
+    }
 })
 
 new Vue({
     el: '#root',
-
 });
 
