00611d5a121734787684e1cbf86c09c53c89ba61
Florian Puget <florian.puget@gmail.com>
2019-11-19 16:57:28 +0100

    ex lesson14 - named slots, with default values which can be overriden

--- main.js
+++ main.js
@@ -1,23 +1,30 @@
-// Using a vue instance to handle events, which is accessible from any component 
-// and allows parent<>child or siblings communication
-window.Event = new Vue();
-
-Vue.component('coupon', {
-    template: `<input placeholder="Enter coupon code" @blur="onCouponApplied">`,
-    methods: {
-        onCouponApplied() {
-            Event.$emit('applied')
-        }
-    }
+Vue.component('modal', {
+    template: `
+        <div class="modal is-active">
+            <div class="modal-background"></div>
+            <div class="modal-card">
+                <header class="modal-card-head">
+                    <p class="modal-card-title">
+                        <slot name='header'></slot>
+                    </p>
+                    <button class="delete" aria-label="close"></button>
+                </header>
+                <section class="modal-card-body">
+                    <slot></slot>
+                </section>
+                <footer class="modal-card-foot">
+                    <slot name='footer'>
+                        <button class="button is-success">Save changes</button>
+                        <button class="button">Cancel</button>
+                    </slot>
+                </footer>
+            </div>
+        </div>
+    `,
 })
 
 new Vue({
     el: '#root',
-    data: {
-        couponApplied: false
-    },
-    created() {
-        Event.$on('applied', () => this.couponApplied = true);
-    }
+
 });
 
